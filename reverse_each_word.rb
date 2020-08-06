8c2ba34fe8fa505f4481514a429a4e79b5c8ff0e
Aaron Montgomery <montaaron11@gmail.com>
2020-08-06 04:12:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
-  array.each do|string|
-    new_array << string.reverse
-  end
-    new_array.collect { |string| string }
+  
 end
   
